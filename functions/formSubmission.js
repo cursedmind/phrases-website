5f5382302b1ac1bb5a2e3df1bf2c3a4a6d41a98b
cursedmind <135279808+cursedmind@users.noreply.github.com>
2023-10-14 11:51:29 +0530

    Update formSubmission.js

--- functions/formSubmission.js
+++ functions/formSubmission.js
@@ -1,1 +1,11 @@
+exports.handler = async (event, context) => {
+  // Process the form submission data here
+  // For example, send an email or save the data to a database
+
+  // Respond with a success message
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Your information is received. We will revert back soon via email." })
+  };
+};
 
