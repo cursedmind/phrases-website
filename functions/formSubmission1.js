38700bdc5c166352b0142c14b992a35f20c8cd18
cursedmind <135279808+cursedmind@users.noreply.github.com>
2023-10-17 01:49:27 +0530

    Update formSubmission1.js

--- functions/formSubmission1.js
+++ functions/formSubmission1.js
@@ -1,7 +1,18 @@
 exports.handler = async function(event, context) {
+  const formData = JSON.parse(event.body);
+
+  const { name, email, docLink } = formData;
+
+  // You can now do something with this data (e.g., save it to a database, send an email, etc.)
+  // For now, let's just log it
+  console.log('Received form 1 submission:');
+  console.log('Name:', name);
+  console.log('Email:', email);
+  console.log('Google Doc Link:', docLink);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Form 1 submitted successfully!" })
+    body: JSON.stringify({ message: 'Form 1 submitted successfully' })
   };
-}
+};
 
