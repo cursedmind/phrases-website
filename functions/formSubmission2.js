0b7ce30410ad40a6b92ddd14765c151516123017
cursedmind <135279808+cursedmind@users.noreply.github.com>
2023-10-17 01:54:24 +0530

    Update formSubmission2.js

--- functions/formSubmission2.js
+++ functions/formSubmission2.js
@@ -1,7 +1,17 @@
+
 exports.handler = async function(event, context) {
+  const formData = JSON.parse(event.body);
+
+  const { interaction } = formData;
+
+  // You can now do something with this data (e.g., save it to a database, send an email, etc.)
+  // For now, let's just log it
+  console.log('Received form 2 submission:');
+  console.log('Interaction:', interaction);
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Form 2 submitted successfully!" })
+    body: JSON.stringify({ message: 'Form 2 submitted successfully' })
   };
-}
+};
 
